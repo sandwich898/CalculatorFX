8185440ea0515b44585b2300d8224371fc8cf547
Dennis <bauer.denni@icloud.com>
2024-01-22 22:16:39 +0100

    Unimportant Files

--- src/main/java/programm/sandwich/calculater/Main.java
+++ src/main/java/programm/sandwich/calculater/Main.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main extends Application {
